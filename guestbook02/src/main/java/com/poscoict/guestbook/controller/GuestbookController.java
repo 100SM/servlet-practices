a95cc27ca0cd9044e73325266a7f5223e6c308cd
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-13 21:13:46 +0900

    guestbook02 final

---
+++ guestbook02/src/main/java/com/poscoict/guestbook/controller/GuestbookController.java
@@ -0,0 +1,72 @@
+package com.poscoict.guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.poscoict.guestbook.dao.GuestbookDao;
+import com.poscoict.guestbook.vo.GuestbookVo;
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String actionName = request.getParameter("a");
+		String page = "";
+		try {
+			if ("form".equals(actionName)) {
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+				rd.forward(request, response);
+			} else if ("add".equals(actionName)) {
+				String name = request.getParameter("name");
+				String password = request.getParameter("password");
+				String message = request.getParameter("message");
+
+				GuestbookVo vo = new GuestbookVo();
+
+				vo.setName(name);
+				vo.setPassword(password);
+				vo.setMessage(message);
+
+				new GuestbookDao().insert(vo);
+
+				response.sendRedirect(request.getContextPath() + "/gb");
+
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+				rd.forward(request, response);
+			} else if ("delete".equals(actionName)) {
+				Long no = Long.parseLong(request.getParameter("no"));
+				String password = request.getParameter("password");
+				new GuestbookDao().delete(no, password);
+				response.sendRedirect(request.getContextPath() + "/gb");
+			} else if ("deleteform".equals(actionName)) {
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp");
+				rd.forward(request, response);
+			} else {
+				GuestbookDao dao = new GuestbookDao();
+				List<GuestbookVo> list = dao.findAll();
+
+				request.setAttribute("list", list);
+
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+				rd.forward(request, response);
+			}
+		} catch (Exception e) {
+			page = "/WEB-INF/views/index.jsp";
+		} finally {
+			page = "/WEB-INF/views/index.jsp";
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
