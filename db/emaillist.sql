ae2efafeb4fc6d3602032e17f4bcd0915b3fcdff
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-13 12:20:38 +0900

    emaillist db sql

---
+++ db/emaillist.sql
@@ -0,0 +1,13 @@
+show tables;
+
+describe emaillist;
+
+-- create
+insert into emaillist values(null, '둘', '리', 'dooly@gmail.com');
+insert into emaillist values(null, '루', '피', 'luffy@gmail.com');
+
+-- read
+select no, first_name, last_name, email from emaillist order by no desc;
+
+-- delete
+delete from emaillist where no = 5;
