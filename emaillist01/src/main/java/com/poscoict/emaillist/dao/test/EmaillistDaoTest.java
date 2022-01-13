4e02e8c3db7e43f1e1277a1007872a988684a590
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-13 11:23:39 +0900

    emaillist01 #finish

--- emaillist01/src/main/java/com/poscoict/emaillist/dao/test/EmaillistDaoTest.java
+++ emaillist01/src/main/java/com/poscoict/emaillist/dao/test/EmaillistDaoTest.java
@@ -8,14 +8,24 @@ import com.poscoict.emaillist.vo.EmaillistVo;
 public class EmaillistDaoTest {
 
 	public static void main(String[] args) {
+//		testInsert();
 		testFindAll();
+	}
+
+	private static void testInsert() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("백");
+		vo.setLastName("성문");
+		vo.setEmail("bsm2716@gmail.com");
+
+		boolean result = new EmaillistDao().insert(vo);
+		System.out.println(result ? "success" : "fail");
 	}
 
 	private static void testFindAll() {
 		List<EmaillistVo> list = new EmaillistDao().findAll();
 		for (EmaillistVo vo : list) {
 			System.out.println(vo);
-			//커밋
 		}
 	}
 }
---
+++ emaillist01/src/main/webapp/add.jsp
@@ -0,0 +1,20 @@
+<%@page import="com.poscoict.emaillist.dao.EmaillistDao"%>
+<%@page import="com.poscoict.emaillist.vo.EmaillistVo"%>
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="EUC-KR"%>
+<%
+request.setCharacterEncoding("utf-8");
+
+String firstName = request.getParameter("fn");
+String lastName = request.getParameter("ln");
+String email = request.getParameter("email");
+
+EmaillistVo vo = new EmaillistVo();
+vo.setFirstName(firstName);
+vo.setLastName(lastName);
+vo.setEmail(email);
+
+new EmaillistDao().insert(vo);
+
+response.sendRedirect("/emaillist01");
+%>
