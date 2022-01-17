e8e08f13b30aa3f2fc7e8f9c3d0c2d9766469cb8
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-17 15:08:25 +0900

    guestbook01 final

---
+++ db/user.sql
@@ -0,0 +1,15 @@
+-- user
+desc user;
+
+-- insert
+insert into user values(null, '관리자', 'admin@mysite.com', '1234', 'male', now());
+
+-- select(login)
+select no, name from user where email = 'admin@mysite.com' and password = '1234';
+
+-- select2(update profile)
+select * from user;
+
+-- update
+update user set name=' ', gender = ' ' where no = 10;
+update user set name=' ', gender = ' ', password = ' ' where no = 10;
